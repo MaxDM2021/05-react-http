e4afd9f889ff37ef5e02612744984a340ad9b8a9
Maksym Dmitriiev <95755128+MaxDM2021@users.noreply.github.com>
2022-11-13 03:43:27 +0300

    eee

--- src/components/PokemonInfo/PokemonInfo.js
+++ src/components/PokemonInfo/PokemonInfo.js
@@ -1,10 +1,15 @@
 import { Component } from "react";
+import PokemonFallbackView from "../PokemonErrorView"
+import PokemonDataView from "../PokemonDataView";
+import PokemonPendingVie from "../PokemonPendingVie";
+import pokemonAPI from "../PokemonApi/PokemonApi";
+
 
 class PokemonInfo extends Component {
   state = {
     pokemon: null,
-    loading: false,
     error: null,
+    status: 'idle',
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -12,49 +17,43 @@ class PokemonInfo extends Component {
     const nextName = this.props.pokemonName;
 
     if (prevName !== nextName) {
-  
+      this.setState({ status: 'pending' });
 
-      this.setState({ loading: true, pokemon: null });
-
-      setTimeout(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            }
-
-            return Promise.reject(
-              new Error(`Нет покемона с именем ${nextName}`)
-            );
-          })
-          .then((pokemon) => this.setState({ pokemon }))
-          .catch((error) => this.setState({ error }))
-          .finally(() => this.setState({ loading: false }));
-      }, 1000);
+      pokemonAPI
+          .fetchPokemon(nextName)
+          .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+          .catch(error => this.setState({ error, status: 'rejected'}))
+    
     }
   }
 
-  render() {
-    const { pokemon, loading, error } = this.state;
-    const { pokemonName } = this.props;
+  // 'idle' - простой
+  // 'pending' - ожидается
+  // 'resolve' - выполнилось с результамом (хорошо)
+  // 'resjected' - отклонено!
 
-    return (
-      <div>
-        {error &&<h1>{error.message}</h1> }
-        {loading && <div>Загружаем...</div>}
-        {!pokemonName && <div>Введите имя покемона.</div>}
-        {pokemon && (
-          <div>
-            <p>{pokemon.name}</p>
-            <img
-              src={pokemon.sprites.other["official-artwork"].front_default}
-              alt=""
-              width="240"
-            />
-          </div>
-        )}
-      </div>
-    );
+  render() {
+    const { pokemon,  error, status } = this.state;
+    const { pokemonName } = this.props;
+  
+
+    if (status === 'idle') {
+      return <div>Введите имя покемона!</div>;
+    }
+
+    if (status === 'pending') {
+      return <PokemonPendingVie pokemonName={pokemonName}/>;
+    }
+
+    if (status === 'rejected') {
+      return <PokemonFallbackView message={error.message}/>
+    }
+
+    if (status === 'resolved') {
+      return (
+<PokemonDataView pokemon={pokemon}/>
+      );
+    }
   }
 }
 
